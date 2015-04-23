f1c77de16871476c38be8b607b113811b49ad683
Yahor Filipchyk <YFilipchyk@thunderhead.com>
2015-04-23 16:42:46 +0300

    Added icons for refs

--- src/main/java/org/yahor/vcs/ui/controllers/RepoTabController.java
+++ src/main/java/org/yahor/vcs/ui/controllers/RepoTabController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
@@ -19,12 +21,17 @@ public class RepoTabController implements Initializable {
 
     @FXML
     private TreeView<String> refs;
+    private Image tagIcon = new Image(getClass().getClassLoader().getResourceAsStream("images/tag.png"));
+    private Image branchIcon = new Image(getClass().getClassLoader().getResourceAsStream("images/branch.png"));
+    private Image remoteIcon = new Image(getClass().getClassLoader().getResourceAsStream("images/remote.png"));
+    private Image localIcon = new Image(getClass().getClassLoader().getResourceAsStream("images/local.png"));
+    private Image tagsIcon = new Image(getClass().getClassLoader().getResourceAsStream("images/tags.png"));
 
     public void loadRepo(Repository repository) throws GitAPIException {
         System.out.println(repository.getAllRefs());
-        TreeItem<String> localBranches = new TreeItem<>("Branches");
-        TreeItem<String> remotes = new TreeItem<>("Remotes");
-        TreeItem<String> tags = new TreeItem<>("Tags");
+        TreeItem<String> localBranches = createTreeItemWithIcon("Branches", localIcon, 16);
+        TreeItem<String> remotes = createTreeItemWithIcon("Remotes", remoteIcon, 16);
+        TreeItem<String> tags = createTreeItemWithIcon("Tags", tagsIcon, 16);
         localBranches.setExpanded(true);
         remotes.setExpanded(true);
         tags.setExpanded(true);
@@ -32,11 +39,11 @@ public class RepoTabController implements Initializable {
                 .map(Map.Entry::getKey)
                 .forEach(refName -> {
                     if (refName.startsWith(Constants.R_TAGS)) {
-                        addRefToTree(tags, refName);
+                        addRefToTree(tags, refName, tagIcon);
                     } else if (refName.startsWith(Constants.R_HEADS)) {
-                        addRefToTree(localBranches, refName);
+                        addRefToTree(localBranches, refName, branchIcon);
                     } else if (refName.startsWith(Constants.R_REMOTES)) {
-                        addRefToTree(remotes, refName);
+                        addRefToTree(remotes, refName, branchIcon);
                     }
                 });
         TreeItem<String> refs = new TreeItem<>("Refs");
@@ -52,7 +59,16 @@ public class RepoTabController implements Initializable {
 
     }
 
-    private static void addRefToTree(TreeItem<String> tree, String refName) {
-        tree.getChildren().add(new TreeItem<>(Repository.shortenRefName(refName)));
+    private static void addRefToTree(TreeItem<String> tree, String refName, Image icon) {
+        tree.getChildren().add(createTreeItemWithIcon(Repository.shortenRefName(refName), icon, 10));
+    }
+
+    private static TreeItem<String> createTreeItemWithIcon(String label, Image icon, int imageHeight) {
+        TreeItem<String> treeItem = new TreeItem<>(label);
+        ImageView imageView = new ImageView(icon);
+        imageView.setPreserveRatio(true);
+        imageView.setFitHeight(imageHeight);
+        treeItem.setGraphic(imageView);
+        return treeItem;
     }
 }
