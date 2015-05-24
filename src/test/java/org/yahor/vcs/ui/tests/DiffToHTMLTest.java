f7f0aadb03ef0aa784651eeea3fbb25e192dec20
Yahor Filipchyk <yahor.filipchyk@gmail.com>
2015-05-24 11:57:07 +0300

    Added unittest

--- src/test/java/org/yahor/vcs/ui/tests/DiffToHTMLTest.java
+++ src/test/java/org/yahor/vcs/ui/tests/DiffToHTMLTest.java
@@ -4,8 +4,21 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.yahor.vcs.ui.git.Repo;
+import org.yahor.vcs.ui.utils.DiffToHTML;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author yahor-filipchyk
@@ -13,19 +26,50 @@ import java.nio.file.Paths;
 public class DiffToHTMLTest {
 
     private static Repo repo;
+    private static final String TEMPLATE = "src/main/resources/diff/diff_template.html";
+    private static final Path TEMPLATE_PATH = Paths.get(getUserDir(), TEMPLATE);
 
     @Test
-    public void testDiffToHTML() {
-
+    public void testDiffToHTML() throws URISyntaxException, IOException {
+        String fileContents = getFileContents(TEMPLATE_PATH);
+        String edited = fileContents.replace("{header}", "diff_template.html");
+        writeToFile(edited, TEMPLATE_PATH.toFile());
+        try {
+            String diff = repo.diffAgainstLatest(TEMPLATE);
+            String html = DiffToHTML.convert(TEMPLATE, diff);
+            assertTrue(String.format("Diff html should contain following hunk %s\nActual was %s", REMOVED_LINE, html),
+                    html.contains(REMOVED_LINE));
+        } finally {
+            writeToFile(fileContents, TEMPLATE_PATH.toFile());
+        }
     }
 
     @BeforeClass
     public static void setup() {
-        repo = Repo.openRepo(Paths.get(System.getProperty("user.dir"), ".git").toFile());
+        repo = Repo.openRepo(Paths.get(getUserDir(), ".git").toFile());
     }
 
     @AfterClass
     public static void tearDown() {
         repo.close();
     }
+
+    private static String getFileContents(Path file) throws URISyntaxException, IOException {
+        return new String(Files.readAllBytes(file), Charset.forName("utf-8"));
+    }
+
+    private static void writeToFile(String contents, File file) throws IOException {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"))) {
+            writer.write(contents);
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
+
+    private static String getUserDir() {
+        return System.getProperty("user.dir");
+    }
+
+    private static final String REMOVED_LINE = "    <pre class=\"source\">-        " +
+            "&lt;span class=&quot;file&quot;&gt;{header}&lt;/span&gt;</pre>";
 }
