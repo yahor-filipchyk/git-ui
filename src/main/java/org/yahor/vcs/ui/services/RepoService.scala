60f36871a0be16d4ec2f0dca973424eddb860c80
Yahor Filipchyk <yahor.filipchyk@gmail.com>
2015-05-27 15:57:29 +0300

    Named tables

--- src/main/java/org/yahor/vcs/ui/services/RepoService.scala
+++ src/main/java/org/yahor/vcs/ui/services/RepoService.scala
@@ -25,10 +25,10 @@ class RepoService(val repo: Repo, val name: String) {
 
   def close(): Unit = repo.close()
 
+  def currentBranch: String = repo.currentBranch
+
   def tags(icon: Image): java.util.List[TreeItem[String]] =
     repo.tags.map(tag => createTreeItemWithIcon(Repository.shortenRefName(tag), icon, 10, false))
-
-  def currentBranch: String = repo.currentBranch
 
   def localBranches(branchIcon: Image, folderIcon: Image): java.util.List[TreeItem[String]] =
     addFoldersAndBranches(new TreeItem[String](""),
